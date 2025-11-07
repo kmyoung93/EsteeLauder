2d8ded77f46b77b3ffa136ebb44c0dcc2146a3f5
혜경 <bboppq@gmail.com>
2025-11-07 18:14:11 +0900

    new 배열 수정

--- products/makeupAll-product-array.js
+++ products/makeupAll-product-array.js
@@ -194,6 +194,8 @@ let newProArray = [
             name: "퓨어 컬러 멜트-온 글로스틱",
             ename: "Pure Color",
             price: 55000,
+            colorOptions:["390 멜티드 멜론","890 멜티드 탠저린", "128 멜티드 블러쉬","156 멜티드 로즈","513 멜티드 모브","655 멜티드 가넷",
+            "185 멜티드 메이플"],
             thmbArray:["makeup1.jpg","makeup2.jpg","makeup3.jpg","makeup4.jpg"]
         },
 
